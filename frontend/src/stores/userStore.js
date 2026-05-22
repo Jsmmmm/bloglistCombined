8235d4504d004b6015a386d553721702a1c1bd0d
Joonas Mäkelä <j00nas_m@hotmail.com>
2026-05-22 17:18:08 +0300

    refactor user localstorage related stuff to its own service persistentuser.js

--- frontend/src/stores/userStore.js
+++ frontend/src/stores/userStore.js
@@ -1,32 +1,37 @@
 import { create } from 'zustand'
 import loginService from '../services/login'
 import blogService from '../services/blogs'
+import {
+  getUser,
+  saveUser,
+  removeUser
+} from '../services/persistentUser'
 
 const useUserStore = create((set) => ({
 
   user: null,
 
-  initUser: async () => {
-    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
-      if (loggedUserJSON) {
-          const user = JSON.parse(loggedUserJSON)
-          set({user})
-          blogService.setToken(user.token)
-      }
+  initUser: () => {
+    const user = getUser()
+    if (user) {
+      blogService.setToken(user.token)
+      set({ user })
+    }
   },
 
   login: async ({ username, password }) => {
-    const user = await loginService.login({ username, password })
-    window.localStorage.setItem(
-      'loggedBlogAppUser', JSON.stringify(user)
-    ) 
+    const user = await loginService.login({
+      username,
+      password
+    })
+    saveUser(user)
     blogService.setToken(user.token)
-    set({user})
+    set({ user })
     return user
   },
 
   logout: () => {
-    window.localStorage.removeItem('loggedBlogAppUser')
+    removeUser()
     blogService.setToken(null)
     set({ user: null })
   }
